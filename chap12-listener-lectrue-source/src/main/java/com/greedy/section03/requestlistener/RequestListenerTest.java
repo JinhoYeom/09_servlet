f1201f8cf3b3a21033ef353a16186a6b481887ba
Jinho Yeom <yeomjh2000@gmail.com>
2023-03-23 10:09:22 +0900

    Servlet 완료

---
+++ chap12-listener-lectrue-source/src/main/java/com/greedy/section03/requestlistener/RequestListenerTest.java
@@ -0,0 +1,36 @@
+package com.greedy.section03.requestlistener;
+
+import javax.servlet.ServletRequestAttributeEvent;
+import javax.servlet.ServletRequestAttributeListener;
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class RequestListenerTest implements ServletRequestListener, ServletRequestAttributeListener {
+
+    public RequestListenerTest() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void requestDestroyed(ServletRequestEvent sre)  { 
+         System.out.println("request destroyed!!");
+    }
+
+    public void attributeRemoved(ServletRequestAttributeEvent srae)  { 
+         System.out.println("request attribute removed!!");
+    }
+
+    public void requestInitialized(ServletRequestEvent sre)  { 
+         System.out.println("request init!!");
+    }
+
+    public void attributeAdded(ServletRequestAttributeEvent srae)  { 
+         System.out.println("request attribute added!!");
+    }
+
+    public void attributeReplaced(ServletRequestAttributeEvent srae)  { 
+         System.out.println("request attribute replaced!!");
+    }
+	
+}
